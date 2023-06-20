1e90496adb6d07df6a52a710995c2abe84c26eb8
Wesley Hughes <wesley_hughes@me.com>
2023-06-20 13:59:12 -0500

    deploy

--- ghostpen/settings.py
+++ ghostpen/settings.py
@@ -57,7 +57,9 @@ REST_FRAMEWORK = {
 # THIS IS NEW
 CORS_ORIGIN_WHITELIST = (
     'http://localhost:3000',
-    'http://127.0.0.1:3000'
+    'http://127.0.0.1:3000',
+    'https://ghostpen-client-9709e86dbb57.herokuapp.com/',
+    'http://ghostpen.live/'
 )
 
 # UPDATE THIS
